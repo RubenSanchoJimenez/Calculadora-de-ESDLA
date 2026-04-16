6a0d4e98e94fa028d16414d18e8fa1b0a13f7a5c
Ruben Sancho <rsancho@mb3-gestion.com>
2026-04-16 18:19:24 +0200

    RSJ - WIP
    Nuevas vistas y funcionalidades

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 ﻿import { AttackController } from './controllers/controlador.js';
+import { CriticoView } from './views/critico.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   AttackController.init();
+  CriticoView.init();
 });
