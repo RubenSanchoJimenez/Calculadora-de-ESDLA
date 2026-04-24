8e95d9deda457c80bbeb67ae4cac343aa75bf619
Ruben Sancho <rsancho@mb3-gestion.com>
2026-04-24 17:33:19 +0200

    RSJ - WIP
    Reestructura de archivos de lógica, se elimina sistema MVC, front casi terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,70 @@
-﻿import { AttackController } from './controllers/controlador.js';
-import { CriticoView } from './views/critico.js';
+﻿const CriticoView = {
+  criticoTipoGcElement: null,
+  criticoTipoRadios: null,
+
+  init() {
+    this.criticoTipoGcElement = document.getElementById('critico_tipo_gc');
+    this.criticoTipoRadios = document.querySelectorAll('input[name="critico_tipo"]');
+
+    if (!this.criticoTipoGcElement || !this.criticoTipoRadios.length) {
+      return;
+    }
+
+    this.criticoTipoRadios.forEach((radio) => {
+      radio.addEventListener('change', () => this.updateVisibility());
+    });
+
+    this.updateVisibility();
+  },
+
+  updateVisibility() {
+    const selectedRadio = Array.from(this.criticoTipoRadios).find((radio) => radio.checked);
+    const shouldShow = selectedRadio && (selectedRadio.id === 'magico_gc' || selectedRadio.id === 'fisico_gc');
+
+    if (shouldShow) {
+      this.criticoTipoGcElement.classList.remove('d-none');
+    } else {
+      this.criticoTipoGcElement.classList.add('d-none');
+    }
+  }
+};
+
+const AttackView = {
+  tamaniosElement: null,
+  dadosInput: null,
+  bonifPosInput: null,
+  bonifNegInput: null,
+  clearButton: null,
+
+  init() {
+    this.tamaniosElement = document.getElementById('tamanios');
+    this.dadosInput = document.getElementById('dadosInput');
+    this.bonifPosInput = document.getElementById('bonifPosInput');
+    this.bonifNegInput = document.getElementById('bonifNegInput');
+    this.clearButton = document.getElementById('clearInputsBtn');
+  },
+
+  update(model) {
+    if (!this.tamaniosElement) {
+      return;
+    }
+
+    if (model.isGarraOrAgarre()) {
+      this.tamaniosElement.classList.remove('d-none');
+    } else {
+      this.tamaniosElement.classList.add('d-none');
+    }
+  },
+
+  clearInputs() {
+    if (this.dadosInput) this.dadosInput.value = '';
+    if (this.bonifPosInput) this.bonifPosInput.value = '';
+    if (this.bonifNegInput) this.bonifNegInput.value = '';
+  }
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   AttackController.init();
   CriticoView.init();
+  AttackView.init();
 });
